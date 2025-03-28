1f72a52ed866870e68f8b128be8c94402f533ebd
steve hocktail <mvboy39@gmail.com>
2025-03-28 01:57:11 -0400

    broke up web.rs into a web.rs get.rs and post.rs

---
+++ src/web/get.rs
@@ -0,0 +1,102 @@
+use crate::config::MPD_ADDRESS;
+use crate::web::{response, static_resources};
+use mpd::{Client, Song};
+use mpd::error::Result;
+
+fn song_string(song: &Song) -> String {
+    let mut to_return = if let Some(title) = &song.title {
+        title
+    } else {
+        &song.file
+    }.clone();
+    if let Some(artist) = &song.artist {
+        to_return.push_str(" by ");
+        to_return.push_str(artist);
+    }
+    to_return
+}
+
+fn response_update() -> Result<String> {
+    let mut mpd = Client::connect(MPD_ADDRESS)?;
+    let current_song = mpd.currentsong()?;
+    let now_playing_string = if let Some(song) = current_song {
+        &song_string(&song)
+    } else {
+        "nothing"
+    };
+
+    let queue = mpd.queue()?;
+    let queue_strings = {
+        let mut qs = vec![];
+        for song in queue {
+            qs.push(song_string(&song));
+        }
+        qs
+    };
+
+    let status = mpd.status()?;
+    let queue_pos = if let Some(pos) = status.song {
+        pos.pos
+    } else {
+        0
+    };
+
+    let current_song_elapsed_time = if let Some(duration) = status.elapsed {
+        duration.as_secs()
+    } else {
+        0
+    };
+
+    let current_song_duration = if let Some(duration) = status.duration {
+        duration.as_secs()
+    } else {
+        0
+    };
+
+    Ok(response::ok(&json::stringify(json::object! {
+        now_playing: now_playing_string,
+        queue: queue_strings,
+        queue_pos: queue_pos,
+        elapsed: current_song_elapsed_time,
+        duration: current_song_duration
+    }), "application/json"))
+}
+
+fn response_all_songs() -> Result<String> {
+    let mut mpd = Client::connect(MPD_ADDRESS)?;
+    let song_file_names = mpd.listall()?;
+    let mut song_list = vec![];
+    for song in song_file_names {
+        let song_with_info = &mpd.lsinfo(song)?[0];
+        let title = if let Some(t) = &song_with_info.title {
+            t
+        } else {
+            "Other"
+        };
+        let artist = if let Some(a) = &song_with_info.artist {
+            a
+        } else {
+            "Other"
+        };
+        let file = song_with_info.file.as_str();
+        song_list.push(json::object! {
+            file: file,
+            title: title,
+            artist: artist
+        });
+    }
+    Ok(response::ok(&json::stringify(song_list), "application/json"))
+}
+
+pub fn handle(head: &str) -> Result<String> {
+    let path_split = head[4..].split_once(" ");
+    match path_split {
+        Some(("/", _)) => Ok(response::ok(static_resources::CONTROL_PANEL, "text/html")),
+        Some(("/style.css", _)) => Ok(response::ok(static_resources::STYLE, "text/css")),
+        Some(("/script.js", _)) => Ok(response::ok(static_resources::SCRIPT, "text/javascript")),
+        Some(("/update", _)) => response_update(),
+        Some(("/allsongs", _)) => response_all_songs(),
+        _ => Ok(response::error("404 Not Found"))
+    }
+}
+
---
+++ src/web/post.rs
@@ -0,0 +1,66 @@
+use crate::config::MPD_ADDRESS;
+use crate::web::response;
+use mpd::{Client, State, Term, Query};
+use mpd::error::Result;
+use std::borrow::Cow;
+
+fn add_song(body: &str) -> Result<String> {
+    let mut mpd = Client::connect(MPD_ADDRESS)?;
+    let mut query = Query::new();
+    mpd.findadd(query.and(Term::File, Cow::from(body)))?;
+    Ok(response::ok_no_content())
+}
+
+fn seek(body: &str) -> Result<String> {
+    let mut mpd = Client::connect(MPD_ADDRESS)?;
+    if let Ok(where_to) = body.trim().parse::<f64>() {
+        mpd.rewind(where_to)?;
+    }
+    // workaround for freeze on skip
+    if mpd.status()?.state == State::Play {
+        mpd.pause(true)?;
+        mpd.play()?;
+    }
+    Ok(response::ok_no_content())
+}
+
+fn previous_song() -> Result<String> {
+    let mut mpd = Client::connect(MPD_ADDRESS)?;
+    mpd.prev()?;
+    // workaround for freeze on skip
+    mpd.pause(true)?;
+    mpd.play()?;
+    Ok(response::ok_no_content())
+}
+
+fn pause() -> Result<String> {
+    let mut mpd = Client::connect(MPD_ADDRESS)?;
+    if mpd.status()?.state == State::Stop {
+        mpd.play()?;
+    } else {
+        mpd.toggle_pause()?;
+    }
+    Ok(response::ok_no_content())
+}
+
+fn next_song() -> Result<String> {
+    let mut mpd = Client::connect(MPD_ADDRESS)?;
+    mpd.next()?;
+    // workaround for freeze on skip
+    mpd.pause(true)?;
+    mpd.play()?;
+    Ok(response::ok_no_content())
+}
+
+pub fn handle(head: &str, body: &str) -> Result<String> {
+    let path_split = head[5..].split_once(" ");
+    match path_split {
+        Some(("/addsong", _)) => add_song(body),
+        Some(("/seek", _)) => seek(body),
+        Some(("/prev", _)) => previous_song(),
+        Some(("/pause", _)) => pause(),
+        Some(("/next", _)) => next_song(),
+        _ => Ok(response::error("404 Not Found"))
+    }
+}
+
